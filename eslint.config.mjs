272321efea588f096325c7ff977e088d674a9d66
Shawn <tiensoonshawnloh@gmail.com>
2026-02-14 13:56:09 +0800

    feat(auth): implement NextAuth configuration with credential provider and user authentication logic
    chore(eslint): add custom ESLint configuration with unused imports handling
    
    feat(proxy): create proxy for NextAuth with custom matcher for authentication routes

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+import unusedImports from "eslint-plugin-unused-imports";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  prettier,
+  ...nextTs,
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+  {},
+]);
+
+export default eslintConfig;
